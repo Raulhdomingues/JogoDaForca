06e9331b88ff7c1728f3bef94247e9c8680963c7
Raul <raulhdomingues@hotmail.com>
2023-05-31 23:00:20 -0300

    newUpdate

--- script.js
+++ script.js
@@ -2,11 +2,24 @@ const tecnologias = [
     "java", "react", "node", "python", "php"
 ];
 const palavraSecreta = tecnologias[Math.floor(Math.random() * tecnologias.length)];
+const letrasErradas = [];
+const letrasCorretas = [];
 
 document.addEventListener("keydown", (evento) => {
     const codigo = evento.keyCode; // 65 - 90 (intervalo)
     if(isLetra(codigo)){
-        //...
+        const letra = evento.key;
+        isLetra(letrasErradas.includes(letra)) {
+            mostrarAvisoLetraRepetida();
+        } else {
+            if(palavraSecreta.includes(letra)){
+                letrasCorretas.push(letra);
+            } else {
+                letrasErradas.push(letra);
+            }
+        }
+
+        // letra faz parte ou nao da palavra
     }
 })
 
