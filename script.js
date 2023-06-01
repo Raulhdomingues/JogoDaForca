1325157778e2f74270df3e1f60fcc87eb0550e41
Raul <raulhdomingues@hotmail.com>
2023-05-31 22:53:04 -0300

    updatejs

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ const tecnologias = [
     "java", "react", "node", "python", "php"
 ];
 const palavraSecreta = tecnologias[Math.floor(Math.random() * tecnologias.length)];
+
+document.addEventListener("keydown", (evento) => {
+    const codigo = evento.keyCode; // 65 - 90 (intervalo)
+    if(isLetra(codigo)){
+        //...
+    }
+})
+
+function isLetra(codigo) {
+    return codigo >= 65 && codigo <= 90;
+}
